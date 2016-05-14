5ff888bd9205ab7be62fb232e26bbd7d89e8de59
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-05-14 14:02:33 +0200

    Initialize project with new starter.
    This starter is based upon 'https://github.com/angular/quickstart', and our own
    gulp-based build system.
    
    BuildSystem:
    Our build system uses; TypeScript, Browserify (with watchify) with gulp.
    It watches for changes and automatically recompiles and reloades the browser.
    
    index.html:
    Moved to res/index.html, removed libraries, as we bundle everything from ts.
    
    package.json:
    Updated package.json with all our buildsystem dependencies, start command, ect.

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './polyfills';
+
+import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {AppComponent} from './app.component';
+
+console.log("Bundle loaded!");
+
+bootstrap(AppComponent);
