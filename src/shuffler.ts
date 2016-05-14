894e7c856c5f369d3da082a7bc2df22d4a1b3d28
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-05-14 21:36:52 +0200

    Added player buttons!

--- src/shuffler.ts
+++ src/shuffler.ts
@@ -3,19 +3,20 @@ import events = require('events');
 
 @Component({
 	selector: 'shuffler',
-	templateUrl: 'shuffler.html',
-	styleUrls: ['../node_modules/font-awesome/css/font-awesome.css']
+	templateUrl: 'shuffler.html'
 })
 
 export class Shuffler
 {
 	private shuffle:boolean;
 	private repeat:boolean;
+    private playing:boolean;
 
 	constructor()
 	{
 		this.shuffle = false;
 		this.repeat = false;
+        this.playing = false;
 	}
 
 	public flip_shuffle(): void
@@ -26,5 +27,10 @@ export class Shuffler
 	public flip_repeat(): void
 	{
 		this.repeat = !this.repeat;
+	}
+
+	public flip_playing(): void
+	{
+		this.playing = !this.playing;
 	}
 }
