9600715b9144ef5a28dce29c03543758b49d11e9
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-05-14 14:49:40 +0200

    Load in template from external file!

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Streamy</h1>'
+    templateUrl: 'app.component.html'
 })
 export class AppComponent { }
+
+
